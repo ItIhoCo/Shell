f576c51c40bea0f5b8b5a31690798a05cf451f89
ItIhoCo <itiho722@gmail.com>
2024-11-24 17:18:52 +0330

    FlushDNS
    FlushDNS For All compouter

---
+++ FlushDNS.ps1
@@ -0,0 +1,16 @@
+﻿$computers=Get-ADComputer -filter * -SearchBase "OU=Domain Object,DC=IHO,DC=co"
+foreach ($comp in $Computers) 
+{
+
+Write-Host $comp.name
+
+Invoke-Command -ComputerName $comp.Name -AsJob {
+
+    ipconfig /flushdns
+    #ipconfig /registerdns
+    #ipconfig /renew
+    #gpupdate /force    
+
+}
+
+}
